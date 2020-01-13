879b4ce792e56041bed0068b533cdaba7e498c6e
James Luttringer <jamesluttringer18@gmail.com>
2020-01-13 15:40:21 -0500

    go

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-    name="lambdata_jamesluttringer2019",
+    name="lambdata-jamesluttringer2019",
     version = "0.1.1",
-    author = "James Luttringer",
+    author = "jamesluttringer2019",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
